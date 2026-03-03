1dedeef6ddce003407053de21654307da7cb42da
aviarqadir4-maker <aviarqadir4@gmail.com>
2026-03-03 08:49:55 +0300

    Delete sw.js

--- sw.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('install', (e) => {
-  console.log('Service Worker Installed');
-});
-
-self.addEventListener('fetch', (e) => {
-  // ئەمە وادەکات ئەپەکە بە ئۆفلاینیش هەندێک شت پیشان بدات
-});
