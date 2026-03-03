44c4b81a17d3bb043a132a2f7864fdfbe10733ef
aviarqadir4-maker <aviarqadir4@gmail.com>
2026-03-03 08:34:30 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,7 @@
+self.addEventListener('install', (e) => {
+  console.log('Service Worker Installed');
+});
+
+self.addEventListener('fetch', (e) => {
+  // ئەمە وادەکات ئەپەکە بە ئۆفلاینیش هەندێک شت پیشان بدات
+});
